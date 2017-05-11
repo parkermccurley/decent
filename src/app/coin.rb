e7bd1a85c109d570b18348373ac71d02bf883325
Parker McCurley <parkermccurlz@gmail.com>
2017-05-10 23:58:31 -0400

    Init APICall; Init test command

--- src/app/coin.rb
+++ src/app/coin.rb
@@ -7,7 +7,18 @@ require "commander/import"
 
 # Application Requirements
 require "../db/setup.rb"
+require "./services/api_call.rb"
 
 program :name, "coin"
 program :version, "0.0.1"
 program :description, "A ruby command line application for managing cryptocurrency holdings."
+
+# Test command to check
+command :test do |c|
+  c.syntax = "coin test"
+  c.description = "execute test command"
+  c.action do |args, options|
+    test_call = APICall.new("https://api.cryptonator.com/api/ticker/btc-usd", "get")
+    puts test_call.call
+  end
+end
--- src/app/services/api_call.rb
+++ src/app/services/api_call.rb
@@ -1,3 +1,17 @@
-class APICall
+require "rest-client"
 
+class APICall
+  def initialize(url, method)
+    @url = url
+    @method = method
+  end
+
+  def call()
+    case @method
+    when 'get'
+      RestClient.get @url
+    when 'post'
+      RestClient.post @url
+    end
+  end
 end
