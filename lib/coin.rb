80c7f86e07e3d5ba8e83fc22695ceedb6c7bdf63
Parker McCurley <parkermccurlz@gmail.com>
2017-05-12 17:36:10 -0400

    fix hodlings

--- lib/coin.rb
+++ lib/coin.rb
@@ -16,5 +16,23 @@ require "./coin/service/bitcoin_exchange_rate_api"
 require "./coin/service/litecoin_exchange_rate_api"
 require "./coin/service/ether_exchange_rate_api"
 
-# Entrypoint Requirement
-require "./coin/program"
+program :name, "coin"
+program :version, "0.0.1"
+program :description, "A ruby command line application for managing cryptocurrency holdings."
+
+command :exchange_rate do |command|
+  command.syntax = "coin exchange_rate"
+  command.description = "Run tasks related to exchange rates"
+  command.option "--currency STRING", String, "Selects currency for task (BTC, LTC, ETH)"
+  command.action do |args, options|
+    ExchangeRateCommand.execute args, options
+  end
+end
+
+command :hodling do |command|
+  command.syntax = "coin hodling"
+  command.description = "Run tasks related to coins you hodl"
+  command.action do |args, options|
+    HodlingCommand.execute args, options
+  end
+end
